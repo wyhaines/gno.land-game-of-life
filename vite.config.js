739b9f11a14b6cd58135b044c3e6f616a992d086
Kirk Haines <wyhaines@gmail.com>
2025-02-28 07:37:44 -0700

    Make the build use relative pathing. Hopefully this fixes gh-pages.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Add this to enforce relative paths
+  build: {
+    outDir: 'dist' // Explicitly set output to dist/ (matches your workflow)
+  },
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
@@ -21,5 +25,4 @@ export default defineConfig({
   define: {
     'process.env': {}
   }
-})
-
+});
